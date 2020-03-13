ff1f5e9df28732e6177226e034ed6745fd68637f
Akmaral <akmaralslmv@gmail.com>
2020-03-13 12:26:43 +0600

    Updates

---
+++ precache-manifest.85519d76c4f523ecf32f25bb35a1cf70.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d64783741b4a4b8e535b4643f7811ca5",
+    "url": "/movies-search-app/index.html"
+  },
+  {
+    "revision": "4d46fe28ebf930904498",
+    "url": "/movies-search-app/static/css/2.1bd2ab2f.chunk.css"
+  },
+  {
+    "revision": "46cc9a251f7de711c828",
+    "url": "/movies-search-app/static/css/main.3a921601.chunk.css"
+  },
+  {
+    "revision": "4d46fe28ebf930904498",
+    "url": "/movies-search-app/static/js/2.4d3728ee.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/movies-search-app/static/js/2.4d3728ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46cc9a251f7de711c828",
+    "url": "/movies-search-app/static/js/main.e7a8ffb3.chunk.js"
+  },
+  {
+    "revision": "cb008fa7bebe0b50f398",
+    "url": "/movies-search-app/static/js/runtime-main.fb557b57.js"
+  }
+]);
